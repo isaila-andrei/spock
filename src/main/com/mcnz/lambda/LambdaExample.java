f5449791b06eddb2266804c680410c74178d31a4
aisaila <isaila_a@yahoo.com>
2022-10-05 00:53:49 +0300

    feature 4

--- src/main/com/mcnz/lambda/LambdaExample.java
+++ src/main/com/mcnz/lambda/LambdaExample.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class LambdaExample {
 
 	public static void main(String[] args) {
+
 		Integer[] numbers = {5, 12, 11, 7};
 
 		Arrays.sort(numbers, new Comparator<Integer>() {
